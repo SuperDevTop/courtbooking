55fe53af6a1e1daf5e10f3abef08eab60eaa7e33
superdev <david.bayne.yo@gmail.com>
2023-11-10 21:51:09 -0800

    34

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const multer = require("multer");
+// const multer = require("multer");
 const path = require("path");
 
 const router = express.Router();
