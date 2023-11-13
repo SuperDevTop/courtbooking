cfd12a3d4a274b3ba8b6e81257eddc5d36782f49
superdev <david.bayne.yo@gmail.com>
2023-11-13 07:28:36 -0800

    1114028

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const cors = require("cors");
 const path = require("path");
 
-mongoose.connect('mongodb://127.0.0.1:27017/tennis_court_booking', {
-// mongoose
-//   .connect(
-//     "mongodb+srv://ctori0816:QwBaTtsIJcRJLTOM@cluster0.psaminp.mongodb.net/tennis_court_booking",
-//     {
+// mongoose.connect('mongodb://127.0.0.1:27017/tennis_court_booking', {
+mongoose
+  .connect(
+    "mongodb+srv://ctori0816:QwBaTtsIJcRJLTOM@cluster0.psaminp.mongodb.net/tennis_court_booking",
+    {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     }
