9c4ed1d59ba794f40244f53756f6a63525c361d8
superdev <david.bayne.yo@gmail.com>
2023-11-13 10:31:02 -0800

    1114330

--- config/url.js
+++ config/url.js
@@ -1,11 +1,11 @@
 let backendUrl;
 
-if (process.env.NODE_ENV === "development") {
-  console.log("development env");
-  backendUrl = "http://localhost:5000";
-} else {
-  console.log(process.env.NODE_ENV);
+// if (process.env.NODE_ENV === "development") {
+//   console.log("development env");
+//   backendUrl = "http://localhost:5000";
+// } else {
+//   console.log(process.env.NODE_ENV);
   backendUrl = "https://courtbooking.vercel.app";
-}
+// }
 
 module.exports = backendUrl;
