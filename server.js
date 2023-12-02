3978b1ddbef150c05634d75b7a55ef594772dc57
superdev <freelancerken2@gmail.com>
2023-12-01 20:13:44 -0800

    1221313

--- server.js
+++ server.js
@@ -99,6 +99,5 @@ io.on("connection", (socket) => {
 
 // Start the server
 const port = process.env.PORT || 5000;
-// server.listen(port, () => console.log(`Server running on port ${port}`));
-app.listen(port, () => console.log(`Server running on port ${port}`));
+server.listen(port, () => console.log(`Server running on port ${port}`));
 console.log(process.env.NODE_ENV);
